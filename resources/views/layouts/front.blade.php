bbd57bf6a8d462f5781860d28f71c19310e6bae9
bertin.dev <color.ui>
2023-07-10 11:59:07 +0100

    update information on store and restore documents user

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -14,7 +14,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    {{--<link rel="stylesheet" href="{{asset('css/bootstrap_4.1.3.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap_4.1.3.css')}}">
     <link rel="stylesheet" href="{{asset('css/all.css')}}">
     <link rel="stylesheet" href="{{asset('css/jquery.dataTables.css')}}">
     <link rel="stylesheet" href="{{asset('css/dataTables.bootstrap4.min.css')}}">
@@ -25,9 +25,9 @@
     <link rel="stylesheet" href="{{asset('css/coreui.css')}}">
     <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
     <link rel="stylesheet" href="{{asset('css/dropzone.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/perfect-scrollbar.css')}}">--}}
+    <link rel="stylesheet" href="{{asset('css/perfect-scrollbar.css')}}">
 
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
+    {{--<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
@@ -38,7 +38,7 @@
     <link href="https://unpkg.com/@coreui/coreui@3.2/dist/css/coreui.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/css/perfect-scrollbar.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/css/perfect-scrollbar.min.css" rel="stylesheet" />--}}
 
     <link href="{{ asset('bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
@@ -277,7 +277,7 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
-{{--<script src="{{asset('js/jquery.js')}}"></script>
+<script src="{{asset('js/jquery.js')}}"></script>
 <script src="{{asset('js/bootstrap.js')}}"></script>
 <script src="{{asset('js/popper.js')}}"></script>
 <script src="{{asset('js/perfect-scrollbar.js')}}"></script>
@@ -298,9 +298,9 @@
 <script src="{{asset('js/bootstrap-datetimepicker.min.js')}}"></script>
 <script src="{{asset('js/select2.full.js')}}"></script>
 <script src="{{asset('js/dropzone.min.js')}}"></script>
-<script src="{{asset('js/jquery.timego.js')}}"></script>--}}
+<script src="{{asset('js/jquery.timego.js')}}"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/perfect-scrollbar.min.js"></script>
@@ -322,7 +322,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.full.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timeago/1.6.5/jquery.timeago.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timeago/1.6.5/jquery.timeago.min.js"></script>--}}
 <script src="{{asset('js/mustache.js')}}"></script>
 <script src="{{asset('js/jquery.notif.js')}}"></script>
 <script src="{{ asset('js/main.js') }}"></script>
@@ -582,13 +582,13 @@
             $('#media_id').attr("value", id);
             $('.document_id').attr("href", id);
             mediaId.attr("href", mediaId.attr("href") + "&mediaId=" + id);
-            $('.mediaArchive').attr("href", id);
+            $('.documentStorage').attr("href", id);
             console.log(id);
             updateMediaTable(id, name);
         });
 
 
-        $('.workflow_validate').click(function() {
+        $('.workflow_validate').click(function () {
             $('.workflow_form').toggle('slow');
         });
 
@@ -906,11 +906,11 @@
             });
         });
 
-        $('.mediaArchive').on('click', function (e) {
-            let fullLoading =  $('#cover-spin');
+        $('.documentStorage').on('click', function (e) {
+            let fullLoading = $('#cover-spin');
             e.preventDefault();
-            let getId = $('.mediaDownload').attr('href');
-            let url = "{{ route('admin.archive.store') }}";
+            let getId = $('.documentStorage').attr('href');
+            let url = "{{ route('store-document') }}";
             $.ajax({
                 url: url,
                 method: 'POST',
@@ -918,13 +918,13 @@
                     id: getId
                 },
                 dataType: 'json',
-                beforeSend: function (){
+                beforeSend: function () {
                     fullLoading.show();
                 },
                 success: function (data) {
                     $('body').notif({
                         title: 'Opération Réussie',
-                        content: data.result,
+                        content: data.status,
                         img: '{{asset('images/success-notif.jpg')}}',
                         cls: 'success1'
                     });
